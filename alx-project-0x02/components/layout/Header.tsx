4e74bc65cd3cbbe1863bc55fc20fddcebe630487
Avellin003 <uavellinbonaparte@gmail.com>
2025-07-22 14:51:49 +0200

    updates

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -1,1 +1,10 @@
-// Header.tsx
+export default function Header(){
+    return(
+        <div className="flex justify-between p-4 w-full">
+            <nav className="flex space-x-4">
+                <a href="/home" className="text-white">Home</a>
+                <a href="/about" className="text-white">About</a>
+            </nav>
+        </div>
+    )
+}
