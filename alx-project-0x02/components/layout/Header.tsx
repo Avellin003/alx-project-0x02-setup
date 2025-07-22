071b88896813d04be70f419b6d978b07df2295ce
Avellin003 <uavellinbonaparte@gmail.com>
2025-07-22 14:53:38 +0200

    updates

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -1,9 +1,15 @@
+import Link from "next/link";
+
 export default function Header(){
     return(
-        <div className="flex justify-between p-4 w-full">
+        <div className="flex justify-between bg-blue-500 p-4 w-full">
             <nav className="flex space-x-4">
-                <a href="/home" className="text-white">Home</a>
-                <a href="/about" className="text-white">About</a>
+                <Link href="/home" className="text-white hover:text-blue-200">
+                    Home
+                </Link>
+                <Link href="/about" className="text-white hover:text-blue-200">
+                    About
+                </Link>
             </nav>
         </div>
     )
